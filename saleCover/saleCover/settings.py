6c9fac33e11e5414e4551aeb13727eeeea8a538a
Aaditya kumar <aadityakumar368a@gmail.com>
2022-11-14 18:53:23 +0530

    third db

--- saleCover/saleCover/settings.py
+++ saleCover/saleCover/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 import os
 import django_heroku
-import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -147,7 +147,7 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR/'staticfiles'
 STATIC_URL = "/static/"
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 SIMPLE_JWT = {
